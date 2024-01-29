14299d3616144d0a538d823103400c91723549a2
rrymar <rrymar@outlook.com>
2024-01-29 17:33:28 +0200

    update for CI build

--- src/InputMapper.jsx
+++ src/InputMapper.jsx
@@ -12,6 +12,7 @@ const fromFields = [
     {name: 'Institution', label: 'Institution'},
     {name: 'Owner', label: 'Owner Person'},
     {name: 'Category', label: 'Category'},
+    {name: 'Expenses', label: 'Expenses'},
     {name: 'Form.Disclosure.InventionDescription', label: 'Form.Disclosure.InventionDescription'},
     {name: 'Form.Disclosure.DisclosedInThePast', label: 'Form.Disclosure.DisclosedInThePast'},
     {name: 'Form.Support.ThirdPartySupport', label: 'Form.Support.ThirdPartySupport'},
@@ -24,16 +25,20 @@ const toFields = [
     {name: "Docket.Field1", label: "Docket.Field1", operators: defaultOperators, valueEditorType: 'select', values: fromFields }, 
     {name: "Docket.Field2", label: "Docket.Field2", operators: defaultOperators, valueEditorType: 'select', values: fromFields }, 
     {name: "Docket.Field3", label: "Docket.Field3", operators: defaultOperators, valueEditorType: 'select', values: fromFields }, 
-    {name: "Docket.Assignee", label: "Docket.Assignee", operators: defaultOperators, valueEditorType: 'select', values: fromFields }  
+    {name: "Docket.Assignee", label: "Docket.Assignee", operators: defaultOperators, valueEditorType: 'select', values: fromFields },  
+    {name: "Docket.ComplexField1", label: "Docket.Assignee", operators: defaultOperators },  
+    {name: "Docket.ComplexField2", label: "Docket.Assignee", operators: defaultOperators }  
 ];
 
 export default function InputMapper() {
     const [query, setQuery] = useState({
         combinator: 'and',
         rules: [
-            {field: 'Docket.Name', operator: '=', value: 'New Docket Item'},
+            {field: 'Docket.Name', operator: '=', value: 'New Docket Item {RecordNumber}'},
             {field: 'Docket.Assignee', operator: '=', value: 'Owner'},
             {field: 'Docket.Field1', operator: '=', value: 'Form.FormName.QuestionName'},
+            {field: 'Docket.ComplexField1', operator: '=', value: 'Expenses / 2 + 100'},
+            {field: 'Docket.ComplexField2', operator: '=', value: 'IF Category = Patent Then AA34 ELSE AB98'},
         ],
     });
 
