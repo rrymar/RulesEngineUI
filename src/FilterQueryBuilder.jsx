79f8e75b368fb84da01bfa85c9aa6aaa041f62a7
rrymar <rrymar@outlook.com>
2024-01-30 12:27:59 +0200

    add more selects

--- src/FilterQueryBuilder.jsx
+++ src/FilterQueryBuilder.jsx
@@ -4,16 +4,28 @@ import {formatQuery, QueryBuilder} from 'react-querybuilder';
 import {QueryBuilderBootstrap} from '@react-querybuilder/bootstrap';
 import 'react-querybuilder/dist/query-builder.css';
 
+const yesNo = [
+    {name: "Y", label: "Yes"},
+    {name: "N", label: "No"}
+]
+
+const institutions = [
+    {name: "1", label: "Org1"},
+    {name: "2", label: "Org2"},
+    {name: "3", label: "Org3"},
+    {name: "4", label: "Org4"},
+]
+
 const fields = [
     {name: 'RecordNumber', label: 'Record Number'},
     {name: 'DisclosureDate', label: 'Disclosure Date'},
-    {name: 'Institution', label: 'Institution'},
+    {name: 'Institution', label: 'Institution',valueEditorType: "select", values: institutions},
     {name: 'Owner', label: 'Owner Person'},
     {name: 'Category', label: 'Category'},
     {name: 'Form.Disclosure.InventionDescription', label: 'Form.Disclosure.InventionDescription'},
     {name: 'Form.Disclosure.DisclosedInThePast', label: 'Form.Disclosure.DisclosedInThePast'},
-    {name: 'Form.Support.ThirdPartySupport', label: 'Form.Support.ThirdPartySupport'},
-    {name: 'Form.Support.IsFundingSupported', label: 'Form.Support.IsFundingSupported'},
+    {name: 'Form.Support.ThirdPartySupport', label: 'Form.Support.ThirdPartySupport', valueEditorType: "select", values: yesNo},
+    {name: 'Form.Support.IsFundingSupported', label: 'Form.Support.IsFundingSupported',valueEditorType: "select", values: yesNo},
     {name: 'Form.FormName.QuestionName', label: 'Form.FormName.QuestionName'}
 ];
 export default function FilterQueryBuilder() {
